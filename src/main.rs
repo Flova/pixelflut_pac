35dc393a40a2bdf97e9000788f8444784e06090b
Florian Vahl <florian@flova.de>
2024-10-04 20:38:15 +0200

    Working version

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,37 @@
-use std::net::TcpStream;
+use clap::Parser;
 use std::io::Write;
 use std::io;
+use std::net::TcpStream;
 
+#[derive(Copy, Clone)]
+struct ColoredSquare {
+    origin: Coordinates,
+    size: u16,
+    color: Color,
+}
 
+#[derive(Copy, Clone)]
 struct Coordinates {
     x: u16,
     y: u16,
 }
 
-struct Color(u8, u8, u8);
+impl std::ops::Add<Coordinates> for Coordinates {
+    type Output = Coordinates;
 
-impl Color {
-    // String representation of the color (hex values of the RGB components)
-    fn to_string(&self) -> String {
-        format!("{:02X}{:02X}{:02X}", self.0, self.1, self.2)
+    fn add(self, other: Coordinates) -> Coordinates {
+        Coordinates {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
     }
+}
+
+#[derive(Copy, Clone)]
+struct Color {
+    r: u8,
+    g: u8,
+    b: u8,
 }
 
 struct Pixel {
@@ -23,27 +40,63 @@ struct Pixel {
 }
 
 impl Pixel {
-    // String representation of the pixel (hex values of the RGB components)
-    fn to_string(&self) -> String {
-        format!("PX {} {} {}\n", self.point.x, self.point.y, self.rgb.to_string())
+    // Implement output function for buffer writing with a
+    fn write<T: Write>(&self, buffer: &mut T) -> io::Result<()> {
+        writeln!(
+            buffer,
+            "PX {x} {y} {r:02x}{g:02x}{b:02x}",
+            x = self.point.x,
+            y = self.point.y,
+            r = self.rgb.r,
+            g = self.rgb.g,
+            b = self.rgb.b
+        )?;
+        Ok(())
     }
 }
 
-fn generate_pixel_string(pixels: &[Pixel]) -> String {
-    let mut pixel_string = String::new();
+/// Command line that sends pixels to a pixelflut server
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Config {
+    #[arg(short, long, default_value = "pixelflut:1234")]
+    url: String,
+    #[arg(default_value = "1000")]
+    width: u16,
+    #[arg(default_value = "1000")]
+    height: u16,
+    #[arg(default_value = "0")]
+    x: u16,
+    #[arg(default_value = "0")]
+    y: u16,
+    #[arg(default_value = "255")]
+    r: u8,
+    #[arg(default_value = "255")]
+    g: u8,
+    #[arg(default_value = "255")]
+    b: u8,
+}
+
+fn generate_pixel_string(pixels: &[Pixel]) -> Vec<u8> {
+    // Allocate byte buffer with matching size
+    let mut pixel_buffer = Vec::new();
+
     for pixel in pixels {
-        pixel_string.push_str(&pixel.to_string());
+        // Mutable borrow string
+        pixel
+            .write(&mut pixel_buffer)
+            .expect("Failed to write pixel");
     }
-    pixel_string
+    pixel_buffer
 }
 
-fn black_square() -> Vec<Pixel> {
+fn serialize_square(square: ColoredSquare) -> Vec<Pixel> {
     let mut pixels = Vec::new();
-    for x in 0..1000 {
-        for y in 0..1000 {
+    for x in 0..square.size {
+        for y in 0..square.size {
             pixels.push(Pixel {
-                point: Coordinates { x, y },
-                rgb: Color(0, 0, 0),
+                point: Coordinates { x, y } + square.origin,
+                rgb: square.color,
             });
         }
     }
@@ -58,24 +111,45 @@ fn send_pixels(ip: &str, pixels: &[Pixel]) -> io::Result<()> {
     let mut stream = TcpStream::connect(ip)?;
     println!("Successfully connected to {}", ip);
 
+    let recv_thread: std::thread::JoinHandle<()> = std::thread::spawn({
+        let mut stream: TcpStream = stream.try_clone().unwrap();
+        move || {
+            std::io::copy(&mut stream, &mut std::io::stderr())
+                .expect("Error sending server answer to stdout.");
+        }
+    });
 
     // Send the string to the server
-    stream.write(pixel_string.as_bytes())?;
+    stream.write_all(pixel_string.as_slice())?;
+    stream.shutdown(std::net::Shutdown::Write).unwrap();
+
+    recv_thread.join().unwrap();
 
     Ok(())
 }
 
-
 fn main() {
     println!("Start pixel client");
 
-    let ip = "[fc42::1]:1234";
+    // Parse the command line arguments
+    let args = Config::parse();
 
     // Generate a black square of 1000x1000 pixels
-    let pixels = black_square();
+    let pixels = serialize_square(ColoredSquare {
+        origin: Coordinates {
+            x: args.x,
+            y: args.y,
+        },
+        size: args.width,
+        color: Color {
+            r: args.r,
+            g: args.g,
+            b: args.b,
+        },
+    });
 
     // Send the pixels to the server
-    match send_pixels(ip, &pixels) {
+    match send_pixels(&args.url, &pixels) {
         Ok(_) => {
             println!("Successfully sent pixels");
         }
